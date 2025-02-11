3460345f729e72fd42d52d0ad658343d381cac13
Rundus <connorfeltman@yahoo.com>
2025-02-11 16:29:48 -0600

    ACES-II code (post UIOWA CDF Operator) commit 18

--- src/Processing/Langmuir/L1_to_L2_langmuir_swept.py
+++ src/Processing/Langmuir/L1_to_L2_langmuir_swept.py
@@ -36,7 +36,7 @@ justPrintFileNames = False
 # 3 -> TRICE II Low Flier
 # 4 -> ACES II High Flier
 # 5 -> ACES II Low Flier
-wRocket = 4
+wRocket = 5
 
 # --- DATA OUTPUT ---
 outputData = True
---
+++ src/Science/LangmuirProbes/toggles.py
@@ -0,0 +1,8 @@
+
+
+class FloatingPotentialToggles:
+
+    # --- FILE I/O ---
+    modifier = ''
+    inputPath_modifier = 'L2'  # e.g. 'L1' or 'L1'. It's the name of the broader input folder
+    outputPath_modifier = 'science\Langmuir'  # e.g. 'L2' or 'Langmuir'. It's the name of the broader output folder
